9dad54fa9a7da8f609a6658a441d5ff8bacd0bff
Rhea Garud <rg921@ic.ac.uk>
2024-12-05 22:01:51 +0000

    implement entropy

--- simulations.py
+++ simulations.py
@@ -1,4 +1,6 @@
 import os
+from collections import defaultdict
+import math
 
 DATA_DIR = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
@@ -19,59 +21,96 @@ def get_word_list_from_file(file_path):
         print(f"An error occurred: {e}")
         return []
 
-def filter_possible_words(words, last_input, accuracy):
-    filtered_words = words
-
+def filter_possible_answers(possible_answers, last_input, accuracy):
+    
     matched_counts = {char: 0 for char in set(last_input)}
 
     for i, (char, acc) in enumerate(zip(last_input, accuracy)):
         if acc == 'g':
-            filtered_words = [word for word in filtered_words if word[i] == char]
+            possible_answers = [word for word in possible_answers if word[i] == char]
             matched_counts[char] += 1
         elif acc == 'y':
-            filtered_words = [
-                word for word in filtered_words
+            possible_answers = [
+                word for word in possible_answers
                 if char in word and word[i] != char and word.count(char) > matched_counts[char]
             ]
             matched_counts[char] += 1
         elif acc == 'x':
-            filtered_words = [
-                word for word in filtered_words
+            possible_answers = [
+                word for word in possible_answers
                 if char not in word or word.count(char) <= matched_counts[char]
             ]
 
-    return filtered_words
+    return possible_answers
 
-def calculate_entropy(word, word_list):
-    return -1
+def generate_correctness_pattern(guess, answer):
+    pattern = []
+    answer_used = [False] * len(answer)
+    
+    for i in range(len(guess)):
+        if guess[i] == answer[i]:
+            pattern.append('g')
+            answer_used[i] = True
+        else:
+            pattern.append(None)
+    
+    for i in range(len(guess)):
+        if pattern[i] is None:
+            if guess[i] in answer and not answer_used[answer.index(guess[i])]:
+                pattern[i] = 'y'
+                answer_used[answer.index(guess[i])] = True
+            else:
+                pattern[i] = 'x'
+    
+    return ''.join(pattern)
 
-def get_guesses(possible_words):
-    scored_words = [(word, calculate_entropy(word, possible_words)) for word in possible_words]
-    scored_words.sort(key=lambda x: x[1], reverse=True)
+def calculate_entropy(guess, possible_answers):
+    pattern_counts = defaultdict(int)
+    
+    for answer in possible_answers:
+        pattern = generate_correctness_pattern(guess, answer)
+        pattern_counts[pattern] += 1
+    
+    entropy = 0
+    for count in pattern_counts.values():
+        probability = count / len(possible_answers)
+        entropy -= probability * math.log2(probability)
+    
+    return entropy
 
-    return scored_words[:3]
+def get_best_guesses(possible_guesses):
+    scored_guesses = [(guess, calculate_entropy(guess, possible_guesses)) for guess in possible_guesses]
+    scored_guesses.sort(key=lambda x: x[1], reverse=True)
+
+    return scored_guesses[:3]
 
 if __name__ == "__main__":
     inputs = []
     accuracy = 'xxxxx'
-    possible_words = get_word_list_from_file(SHORT_WORD_LIST_FILE)
-    guesses = []
+    possible_answers = get_word_list_from_file(SHORT_WORD_LIST_FILE)
+    best_guesses = []
 
-    print("Welcome to Wordle Helper!")
+    print("Welcome to Wordle Solver!")
 
     while len(inputs) < 6 and accuracy != 'ggggg':
         print(f"\nATTEMPT {len(inputs) + 1}")
-        guesses = get_guesses(possible_words)
+        best_guesses = get_best_guesses(possible_answers)
 
-        print(f"Number of possible answers: {len(possible_words)}")
-        print("Suggested words:")
-        for (i, (word, entropy)) in enumerate(guesses):
-            print(f"Word {i + 1}: '{word}', Entropy: {entropy}")
+        if len(best_guesses) == 1:
+            print(f"The answer is '{best_guesses[0][0]}'")
+            inputs.append(best_guesses[0][0])
+            accuracy = 'ggggg'
 
-        inputs.append(input("\nWhich word did you choose?\n"))
-        accuracy = input("\nEnter the corresponding correctness for each letter: \n('g' for a green letter, 'y' for yellow letter and 'x' for a grey one)\n")
+        else:
+            print(f"Number of possible answers: {len(possible_answers)}")
+            print("Suggested words:")
+            for (i, (guess, entropy)) in enumerate(best_guesses):
+                print(f"Word {i + 1}: '{guess}', Entropy: {entropy}")
 
-        possible_words = filter_possible_words(possible_words, inputs[-1], accuracy)
+            inputs.append(input("\nWhich word did you guess?\n"))
+            accuracy = input("\nEnter the corresponding correctness for each letter: \n('g' for a green letter, 'y' for yellow letter and 'x' for a grey one)\n")
+
+            possible_answers = filter_possible_answers(possible_answers, inputs[-1], accuracy)
 
     if (accuracy == 'ggggg'):
         print(f"\nWell done for getting the answer on attempt number {len(inputs)}!")
